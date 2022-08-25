e215bc6d2959cb556a21ced6ec06bb8247e78558
Anando Ghose <anandoghose@gmail.com>
2022-08-25 21:28:43 +0530

    moved footer icons to public

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,23 @@
-
-<div class="grid justify-items-center gap-y-2 py-2">
-    <div>
-        <img src='/src/images/footer/whatsapp.svg' alt='whatsapp icon' width="14" height="14"/>
-        <img src='/src/images/footer/instagram.svg' alt='whatsapp icon' width="14" height="14"/>
-        <img src='/src/images/footer/youtube.svg' alt='whatsapp icon' width="19.87" height="14"/>
+<footer class="grid justify-items-center gap-y-4 py-4 bg-gradient-to-r from-[#89E0E7] via-[#FBBAB8] to-[#FCE3B5] mt-6">
+    <div class="flex justify-center items-center gap-x-4">
+        <img
+            src="/whatsapp.svg"
+            alt="whatsapp icon"
+            width="14"
+            height="14"
+        />
+        <img
+            src="/instagram.svg"
+            alt="whatsapp icon"
+            width="14"
+            height="14"
+        />
+        <img
+            src="/youtube.svg"
+            alt="whatsapp icon"
+            width="19.87"
+            height="14"
+        />
     </div>
-
-</div>
+<p>© 2022. All Rights Reserved</p>
+</footer>
