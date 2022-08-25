46172b2dd6d558f3d14972de3381b7119302422c
Anando Ghose <anandoghose@gmail.com>
2022-08-25 21:23:53 +0530

    missing footer icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,9 @@
 
 <div class="grid justify-items-center gap-y-2 py-2">
     <div>
-        <img src='src/images/footer/whatsapp.svg' alt='whatsapp icon' width="14" height="14"/>
-        <img src='src/images/footer/instagram.svg' alt='whatsapp icon' width="14" height="14"/>
-        <img src='src/images/footer/youtube.svg' alt='whatsapp icon' width="19.87" height="14"/>
+        <img src='/src/images/footer/whatsapp.svg' alt='whatsapp icon' width="14" height="14"/>
+        <img src='/src/images/footer/instagram.svg' alt='whatsapp icon' width="14" height="14"/>
+        <img src='/src/images/footer/youtube.svg' alt='whatsapp icon' width="19.87" height="14"/>
     </div>
 
 </div>
