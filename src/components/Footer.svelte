5e40bfa6949069fa67c8c70bfc8263b0655e3e18
Anando Ghose <anandoghose@gmail.com>
2022-08-31 11:55:58 +0530

    social links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,23 +1,16 @@
-<footer class="grid justify-items-center gap-y-4 py-4 bg-gradient-to-r from-[#89E0E7] via-[#FBBAB8] to-[#FCE3B5] mt-6">
-    <div class="flex justify-center items-center gap-x-4">
-        <img
-            src="/whatsapp.svg"
-            alt="whatsapp icon"
-            width="14"
-            height="14"
-        />
-        <img
-            src="/instagram.svg"
-            alt="whatsapp icon"
-            width="14"
-            height="14"
-        />
-        <img
-            src="/youtube.svg"
-            alt="whatsapp icon"
-            width="19.87"
-            height="14"
-        />
-    </div>
-<p>© 2022. All Rights Reserved</p>
+<footer
+  class="grid justify-items-center gap-y-4 py-4 bg-gradient-to-r from-[#89E0E7] via-[#FBBAB8] to-[#FCE3B5] mt-6"
+>
+  <div class="flex justify-center items-center gap-x-4">
+    <a href="https://wa.me/919940419888">
+      <img src="/whatsapp.svg" alt="whatsapp icon" width="14" height="14" />
+    </a>
+    <a href="https://www.instagram.com/sushi.yogi/">
+      <img src="/instagram.svg" alt="whatsapp icon" width="14" height="14" />
+    </a>
+    <a href="https://www.youtube.com/channel/UCRxo3M_LHM88IPCBCwbE0CQ">
+      <img src="/youtube.svg" alt="whatsapp icon" width="19.87" height="14" />
+    </a>
+  </div>
+  <p>© 2022. All Rights Reserved</p>
 </footer>
