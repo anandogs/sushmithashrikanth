9cce034a08bd5923217d31341541751b5537d35a
Anando Ghose <anandoghose@gmail.com>
2022-11-08 16:52:39 +0530

    slideshow

--- src/components/ImageCarousel.svelte
+++ src/components/ImageCarousel.svelte
@@ -11,20 +11,31 @@ import image8 from '../images/slideshow/8.png';
 const imageList = [image1, image2, image3, image4, image5, image6, image7, image8];
 
 import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+  import { onMount } from 'svelte';
+let showArrows = true
 
+onMount(() => {
+  let windowWidth = window.innerWidth;
+  if (windowWidth < 768) {
+    showArrows = false
+  }
+});
 
 </script>
 
 <section class="w-screen">
-    <div class="md:w-[80%] m-auto">
-    <Splide aria-label="My Favorite Images">
-        {#each imageList as image}
-            <SplideSlide>
+    <div class="md:w-[80%] w-full md:m-auto">
+    <Splide options={{arrows: showArrows, autoplay:true, interval:4000, rewind:true,}} aria-label="Carousel Images">
+      
+      {#each imageList as image}
+            
+        <SplideSlide>
                 <!-- svelte-ignore a11y-img-redundant-alt -->
                 <img src={image} alt="Carousel Image"/>
             </SplideSlide>
         {/each}
-      </Splide>
+      
+    </Splide>
       
     </div>
 
